173103733b0b4cfab9c621320eb860b345a77d6b
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-23 23:30:00 -0500

    Updated code repo.

---
+++ CalculatorCommand/CalculatorCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CalculatorCommand {
+	class CalculatorCommand:Command {
+		private char _operator;
+		private int _operand;
+		private Calculator _calculator;
+
+		//C'tor
+		public CalculatorCommand(Calculator calculator, char @operator, int operand) {
+			this._calculator = calculator;
+			this._operator = @operator;
+			this._operand = operand;
+		}
+
+		//Gets operator
+		public char Operator {
+			set { _operator = value; }
+		}
+		//Get operand
+		public int Operand {
+			set { _operand = value; }
+		}
+		public override void Execute() {
+			_calculator.Operation(_operator, _operand);
+		}
+
+		public override void UnExecute() {
+			_calculator.Operation(Undo(_operator),_operand);
+		}
+
+		private char Undo(char @operator) {
+			switch (@operator) {
+				case '+': return '-';
+				case '-': return '+';
+				case '*': return '/';
+				case '/': return '*';
+				default: throw new ArgumentException("@operator");
+			}
+		}
+	}
+}
---
+++ CalculatorCommand/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCommand {
+	class MainApp {
+		static void Main() {
+			//Create user and let her computer
+			User user = new User();
+
+			//user presses calculator buttons
+			user.Compute('+', 100);
+			user.Compute('-', 50);
+			user.Compute('*', 10);
+			user.Compute('/', 2);
+			
+			//Undo four commands
+			user.Undo(4);
+
+			//redo 3 commands
+			user.Redo(3);
+			
+			//Wait for user
+			Console.ReadKey();
+		}
+	}
+}
